8fe635593d4d71e4713735c625325fe098f41d49
AlexanderMelian <alexandermelian@outlook.com>
2021-04-18 13:15:10 -0300

    add

---
+++ microsoftLearn/tests/vectorFunctions.rs
@@ -0,0 +1,10 @@
+fn solution(s: Vec<char>){
+    println!("{:?}",s);
+}
+
+
+fn main(){
+    let param: Vec<char> = vec!['a', 'b', 'c', 'd', 'e'];
+    println!("{:?}", param);
+    solution(param);
+}
--- practices/reverseString.rs
+++ practices/reverseString.rs
@@ -1,12 +1,24 @@
+//https://leetcode.com/problems/reverse-string/
+
+struct Solution;
+
 impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
-        
+        let s_len = s.len();
+        let mut help;
+            let mut i = 0;
+            while i < (s_len/2){
+                help = s[i];
+                s[i] = s[s_len-1-i];
+                s[s_len-1-i] = help;
+                i += 1;
+        }
     }
 }
 
 
 fn main(){
-    struct Solution{
-        
-    }
+    let mut param: Vec<char> = vec!['a','e'];
+    Solution::reverse_string(&mut param);
+    println!("{:?}",param);
 }
